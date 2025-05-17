9f59201f4ab621ed2e7cdcea4b6bc8c0076e1787
ishandesaii <ishandesai1202@gmail.com>
2025-05-16 20:16:37 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// vite.config.ts
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
-  // ...rest of config
 });
-
